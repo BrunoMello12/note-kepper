b62bebbecd279b20b6987c29f6b003afcf3d6ce6
Bruno <Cliente@DESKTOP-2TBK80U>
2023-09-11 23:51:43 -0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListarNotasComponent } from './components/notas/listar-notas/listar-notas.component';
+import { CardNotaComponent } from './components/notas/card-nota/card-nota.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ListarNotasComponent,
+    CardNotaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
